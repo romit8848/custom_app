273d6981c6f84b9c6e4dbb481fbae8507dc0e266
frappe <frappe@ubuntu>
2022-07-22 06:59:18 +0000

    feat: Initialize App

---
+++ custom_app/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Custom App",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Custom App")
+		}
+	]
